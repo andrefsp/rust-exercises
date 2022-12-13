8f989ff888dc24a54cdc2c928163286cbb384300
andrefsp <andrefsp@gmail.com>
2022-12-13 21:16:06 +0000

    add extra methods

--- src/list/list.rs
+++ src/list/list.rs
@@ -2,33 +2,66 @@ pub fn one() -> u8 {
     1
 }
 
-enum Value {
-    Element { content: u8, next: Box<Value> },
+pub enum Node {
+    Element { content: u8, next: Box<Node> },
     Nil,
 }
 
-impl Value {
-    fn new(val: u8) -> Value {
-        Value::Element {
+impl Node {
+    pub fn new(val: u8) -> Node {
+        Node::Element {
             content: val,
-            next: Box::new(Value::Nil),
+            next: Box::new(Node::Nil),
+        }
+    }
+
+    pub fn next_node(&self) -> Option<Node> {
+        /*
+         *  XXX(andrefsp): we must turn Node into a reference Rc.
+         *
+        match self {
+            Node::Element { content: _, next } => Some(*next),
+            Node::Nil => None,
+        }
+        */
+        None
+    }
+
+    pub fn set_next(&mut self, n: Node) {
+        if let Node::Element { content: _, next } = self {
+            *next = Box::new(n)
+        };
+    }
+
+    pub fn get_value(&self) -> Option<u8> {
+        match self {
+            Node::Element { content, next: _ } => Some(*content),
+            Node::Nil => None,
         }
     }
 }
 
 pub struct List {
-    top: Value,
+    top: Box<Node>,
 }
 
 impl List {
     pub fn new() -> List {
-        List { top: Value::Nil }
+        List {
+            top: Box::new(Node::Nil),
+        }
     }
 
-    pub fn add(&mut self, val: u8) {
-        if let Value::Nil = self.top {
-            let elem = Value::new(val);
-            self.top = elem;
+    pub fn push(&mut self, val: u8) {
+        if let Node::Nil = *self.top {
+            self.top = Box::new(Node::new(val));
+            return;
         }
+
+        let mut elem = Node::new(val);
+
+        //elem.set_next(*self.top)
+
+        //* self.top = elem;
     }
 }
--- src/list/test.rs
+++ src/list/test.rs
@@ -1,10 +1,25 @@
 use super::list::List;
+use super::list::Node;
 
 #[test]
-fn test_check_this() {
+fn test_value_next() {
     let mut l = List::new();
 
-    l.add(1);
-    l.add(3);
-    l.add(2);
+    l.push(1);
+    l.push(3);
+    l.push(2);
+}
+
+#[test]
+fn test_get_value() {
+    let v1 = Node::new(1);
+    assert_eq!(v1.get_value().unwrap(), 1);
+}
+
+#[test]
+fn test_set_next() {
+    let mut v1 = Node::new(1);
+    let v2 = Node::new(2);
+
+    v1.set_next(v2);
 }
